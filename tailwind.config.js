05fe5679c4e9627f2747e60252ec227b6c3f9df2
NabiL Ashbat <nbl.dev21@gmail.com>
2023-03-30 22:28:08 +0300

    chore(project): minor tailwind.config improvments

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,34 +1,33 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    // * not being used!
+    // "./app/**/*.{js,ts,jsx,tsx}",
+    // "./pages/**/*.{js,ts,jsx,tsx}",
+    // "./components/**/*.{js,ts,jsx,tsx}",
 
-    // Or if using `src` directory:
-    "./src/**/*.{js,ts,jsx,tsx}",
+    // Including `src` directory:
+    "./src/**/*.{js,jsx}",
   ],
   theme: {
-    extend: {
-      fontFamily: {
-        poppins: ["Poppins", "sans-serif"],
-      },
-      // Project custom colors
-      colors: {
-        black: "#000000",
-        clay: "#F1F6FA",
-        cyanaqua: "#90EEE1",
-        darkpurple: "#32314D",
-        iceblue: "#F1F6FA",
-        purple: "#7874F2",
-        "purple-almostblack": "#32314D",
-        "purple-dark": "#585785",
-        "purple-light": "#7874F2",
-        pumpkin: "#FF8A57",
-        skyblue: "#1B96EF",
-        white: "#FFFFFF",
-      },
-      plugins: [],
+    // Project custom colors
+    colors: {
+      black: "#000000",
+      clay: "#F1F6FA",
+      cyanaqua: "#90EEE1",
+      darkpurple: "#32314D",
+      iceblue: "#F1F6FA",
+      purple: "#7874F2",
+      "purple-almostblack": "#32314D",
+      "purple-dark": "#585785",
+      "purple-light": "#7874F2",
+      pumpkin: "#FF8A57",
+      skyblue: "#1B96EF",
+      white: "#FFFFFF",
+    },
+    fontFamily: {
+      poppins: ["Poppins", "sans-serif"],
     },
   },
+  plugins: [],
 }
