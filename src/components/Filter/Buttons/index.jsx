a1786fb33698c1374905f36b3b711104d1b38719
Abobakr Mahdi <93738112+BKR779@users.noreply.github.com>
2023-04-02 12:16:36 +0300

    feat(pricefilter): add a price filter component style
    add a new component called price filter witch contain a product filter that filters the products by
    thier range in to ways the first way by the slider range the second way by the input way
    
    BREAKING CHANGE: delet the photo that i made it ot replace the price filter for a short time in the
    past to finish style the hole filter componentand change one line in the button component that
    remove the image and replace it with the real price filter

--- src/components/Filter/Buttons/index.jsx
+++ src/components/Filter/Buttons/index.jsx
@@ -1,5 +1,7 @@
 import Image from "next/image"
 
+import PriceFilter from "../Price"
+
 export default function Buttons({ filters = [], handleCategoryFilter }) {
   const CategoryFilter = ({ filter, image, alt }) => (
     <button className='ml-4' onClick={() => handleCategoryFilter(filter)}>
@@ -26,7 +28,7 @@ export default function Buttons({ filters = [], handleCategoryFilter }) {
         </span>
       </div>
       <div className='flex flex-col lg:flex-row justify-between shadow-2xl rounded-[8vw] lg:rounded-full items-center lg:pr-20 py-10'>
-        <span className='grid grid-cols-4 gap-4 md:gap-28 lg:gap-4 pb-10 lg:pb-0 mx-auto pr-4 lg:px-0'>
+        <span className='grid grid-cols-4 gap-4 md:gap-20 lg:gap-4 pb-10 lg:pb-0 mx-auto pr-4 lg:px-0'>
           {filters.map(({ filter, image, alt }) => (
             <CategoryFilter
               key={filter}
@@ -36,13 +38,8 @@ export default function Buttons({ filters = [], handleCategoryFilter }) {
             />
           ))}
         </span>
-        <span className='w-[70vw] md:w-[40vw] lg:w-[400px]'>
-          <Image
-            src='/Images/Price-Filter.png'
-            alt='...'
-            width={400}
-            height={400}
-          />
+        <span className='w-[70vw] md:w-[40vw] lg:w-[400px] lg:mb-[3vw]'>
+          <PriceFilter />
         </span>
       </div>
     </div>
--- src/components/Filter/Price/index.jsx
+++ src/components/Filter/Price/index.jsx
@@ -1,3 +1,139 @@
+import { useState } from "react"
+import { Range } from "react-range"
+
 export default function PriceFilter() {
-  return <div>PriceFilter</div>
+  const [tempValues, setTempValues] = useState([270, 770]) // temporary min and max values for changes made by user
+
+  const handleChange = (newValues) => {
+    setTempValues(newValues)
+  }
+
+  const handleMinChange = (e) => {
+    const newMin = +e.target.value.replace(/\D/g, "")
+    if (newMin < 0) {
+      alert("Sorry, the minimum price is $10")
+    } else if (newMin > tempValues[1]) {
+      alert("Sorry, the minimum price cannot be greater than the maximum price")
+    } else {
+      setTempValues([newMin, tempValues[1]])
+    }
+  }
+
+  const handleMaxChange = (e) => {
+    const newMax = +e.target.value.replace(/\D/g, "")
+    if (newMax > 1000) {
+      alert("Sorry, the maximum price is $1000")
+    } else if (newMax < tempValues[0]) {
+      alert("Sorry, the maximum price cannot be less than the minimum price")
+    } else {
+      setTempValues([tempValues[0], newMax])
+    }
+  }
+
+  return (
+    <div className='flex flex-col items-stretch'>
+      <div className='flex justify-around text-2xl text-bubble-gum font-poppins font-extrabold ml-5 md:ml-10'>
+        <input
+          type='text'
+          value={`$${tempValues[0]}`}
+          onChange={handleMinChange}
+          className='w-24 rounded-md px-2 py-1 outline-none focus:outline-[#FF8A57] focus:outline-offset-2'
+        />
+        <input
+          type='text'
+          value={`$${tempValues[1]}`}
+          onChange={handleMaxChange}
+          className='w-24 rounded-md px-2 py-1 outline-none focus:outline-[#FF8A57] focus:outline-offset-2'
+        />
+      </div>
+      <div className='mt-4 mx-4 md:mx-8 w-[90%]'>
+        <Range
+          step={1}
+          min={0}
+          max={1000}
+          values={tempValues}
+          onChange={handleChange}
+          renderTrack={({ props, children }) => (
+            <div {...props} className='py-1.5 bg-[#FF8A57] rounded-full'>
+              {children}
+            </div>
+          )}
+          renderThumb={({ props }) => (
+            <div
+              {...props}
+              className='bg-[#7874F2] p-3 rounded-full outline-none cursor-pointer'
+            />
+          )}
+        />
+      </div>
+    </div>
+  )
 }
+
+/* Just leave it i need to deal with it in another time */
+
+// import Slider from "@material-ui/core/Slider"
+// import { useState } from "react"
+
+// export default function PriceFilter() {
+//   const [tempValues, setTempValues] = useState([270, 770]) // temporary min and max values for changes made by user
+
+//   const handleChange = (event, newValues) => {
+//     setTempValues(newValues)
+//   }
+
+//   const handleMinChange = (event, newMin) => {
+//     if (newMin < 10) {
+//       alert("Sorry, the minimum price is $10")
+//     } else if (newMin > tempValues[1]) {
+//       alert("Sorry, the minimum price cannot be greater than the maximum price")
+//     } else {
+//       setTempValues([newMin, tempValues[1]])
+//     }
+//   }
+
+//   const handleMaxChange = (event, newMax) => {
+//     if (newMax > 1000) {
+//       alert("Sorry, the maximum price is $1000")
+//     } else if (newMax < tempValues[0]) {
+//       alert("Sorry, the maximum price cannot be less than the minimum price")
+//     } else {
+//       setTempValues([tempValues[0], newMax])
+//     }
+//   }
+
+//   return (
+//     <div className='flex flex-col items-stretch'>
+//       <div className='flex justify-around text-2xl text-bubble-gum font-poppins font-extrabold ml-5 md:ml-10'>
+//         <input
+//           type='text'
+//           value={`$${tempValues[0]}`}
+//           onChange={handleMinChange}
+//           className='w-24 rounded-md px-2 py-1 outline-none focus:outline-[#FF8A57] focus:outline-offset-2'
+//         />
+//         <input
+//           type='text'
+//           value={`$${tempValues[1]}`}
+//           onChange={handleMaxChange}
+//           className='w-24 rounded-md px-2 py-1 outline-none focus:outline-[#FF8A57] focus:outline-offset-2'
+//         />
+//       </div>
+//       <div className='mt-4 mx-4 md:mx-8 w-[90%]'>
+//         <Slider
+//           value={tempValues}
+//           onChange={handleChange}
+//           step={1}
+//           min={0}
+//           max={1000}
+//           aria-labelledby='range-slider'
+//           disableRipple
+//           classes={{
+//             thumb: "bg-[#7874F2] p-3 rounded-full outline-none cursor-pointer",
+//             rail: "py-1.5 bg-[#FF8A57] rounded-full",
+//             track: "py-1.5 bg-[#FF8A57] rounded-full",
+//           }}
+//         />
+//       </div>
+//     </div>
+//   )
+// }
