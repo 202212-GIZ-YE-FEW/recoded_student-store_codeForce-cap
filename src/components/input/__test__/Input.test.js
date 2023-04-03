cbd7d8d2f52fab0c072f091a392ee305dfec4133
AASB7 <wwwwbasingab@gmail.com>
2023-04-04 00:13:11 +0530

    test(input): fix the error

--- src/components/input/__test__/Input.test.js
+++ src/components/input/__test__/Input.test.js
@@ -1,6 +1,6 @@
 import renderer from "react-test-renderer"
 
-import Input from "../input"
+import Input from "../Input"
 
 it("renders correctly", () => {
   const tree = renderer.create(<Input />).toJSON()
