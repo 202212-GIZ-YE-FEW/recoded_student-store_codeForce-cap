791e74d52581e73d317675c680dd5c82ed9fbbab
messam10 <memaq1998.10.7@gmail.com>
2023-04-04 01:49:41 +0300

    style(photosgallery): implement the CSS

--- src/components/PhotosGallery/PhotosGallery.jsx
+++ src/components/PhotosGallery/PhotosGallery.jsx
@@ -4,33 +4,33 @@ import Image from "next/image"
 
 function PhotosGallery() {
   return (
-    <div>
+    <div className='flex flex-col lg:flex-col md:flex-row w-full'>
       <div>
         <Image
-          className='h-full w-full max-w-full object-cover'
+          className='h-full w-full lg:w-full lg:h-full md:w-96 md:h-60 object-cover'
           src='photo-one.svg'
           width={500}
           height={500}
           alt='Photo-one'
         />
       </div>
-      <div>
+      <div className='flex justify-between lg:flex-row md:flex-col'>
         <Image
-          className='h-full w-full object-cover'
+          className='w-full object-cover'
           src='photo-two.svg'
           width={100}
           height={100}
           alt='photo-two'
         />
         <Image
-          className='h-full w-full object-cover'
+          className='w-full lg:w-full md:w-60 object-cover'
           src='photo-three.svg'
           width={100}
           height={100}
           alt='photo-three'
         />
         <Image
-          className='h-full w-full object-cover'
+          className='w-full object-cover'
           src='photo-four.svg'
           width={100}
           height={100}
