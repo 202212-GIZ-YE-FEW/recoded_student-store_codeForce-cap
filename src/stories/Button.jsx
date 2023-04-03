597c8a6afdc5b7485192d367f6c45db44a116b3c
NabiL Ashbat <nbl.dev21@gmail.com>
2023-04-03 22:53:09 +0300

    build: fix errors after updating jest

--- src/stories/Button.jsx
+++ src/stories/Button.jsx
@@ -1,16 +1,20 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import './button.css';
+import React from "react"
+import PropTypes from "prop-types"
+import "./button.css"
 
 /**
  * Primary UI component for user interaction
  */
 export const Button = ({ primary, backgroundColor, size, label, ...props }) => {
-  const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
+  const mode = primary
+    ? "storybook-button--primary"
+    : "storybook-button--secondary"
   return (
     <button
-      type="button"
-      className={['storybook-button', `storybook-button--${size}`, mode].join(' ')}
+      type='button'
+      className={["storybook-button", `storybook-button--${size}`, mode].join(
+        " "
+      )}
       {...props}
     >
       {label}
@@ -20,8 +24,8 @@ export const Button = ({ primary, backgroundColor, size, label, ...props }) => {
         }
       `}</style>
     </button>
-  );
-};
+  )
+}
 
 Button.propTypes = {
   /**
@@ -35,7 +39,7 @@ Button.propTypes = {
   /**
    * How large should the button be?
    */
-  size: PropTypes.oneOf(['small', 'medium', 'large']),
+  size: PropTypes.oneOf(["small", "medium", "large"]),
   /**
    * Button contents
    */
@@ -44,11 +48,11 @@ Button.propTypes = {
    * Optional click handler
    */
   onClick: PropTypes.func,
-};
+}
 
 Button.defaultProps = {
   backgroundColor: null,
   primary: false,
-  size: 'medium',
+  size: "medium",
   onClick: undefined,
-};
+}
--- src/stories/Button.stories.js
+++ src/stories/Button.stories.js
@@ -1,41 +1,41 @@
-import { Button } from './Button';
+import { Button } from "./Button"
 
 // More on how to set up stories at: https://storybook.js.org/docs/7.0/react/writing-stories/introduction
 export default {
-  title: 'Example/Button',
+  title: "Example/Button",
   component: Button,
-  tags: ['autodocs'],
+  tags: ["autodocs"],
   argTypes: {
     backgroundColor: {
-      control: 'color',
+      control: "color",
     },
   },
-};
+}
 
 // More on writing stories with args: https://storybook.js.org/docs/7.0/react/writing-stories/args
 export const Primary = {
   args: {
     primary: true,
-    label: 'Button',
+    label: "Button",
   },
-};
+}
 
 export const Secondary = {
   args: {
-    label: 'Button',
+    label: "Button",
   },
-};
+}
 
 export const Large = {
   args: {
-    size: 'large',
-    label: 'Button',
+    size: "large",
+    label: "Button",
   },
-};
+}
 
 export const Small = {
   args: {
-    size: 'small',
-    label: 'Button',
+    size: "small",
+    label: "Button",
   },
-};
+}
