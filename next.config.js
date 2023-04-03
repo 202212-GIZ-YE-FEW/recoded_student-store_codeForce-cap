0ccc52686691bab895368babe8ca866d236de15f
NabiL Ashbat <nbl.dev21@gmail.com>
2023-04-03 18:08:39 +0300

    build(next.config): a modify to solve the storybook failure.

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
+const withCSS = require("@zeit/next-css")
 const { i18n } = require("./next-i18next.config")
 
-module.exports = {
+module.exports = withCSS({
   i18n,
   eslint: {
     dirs: ["src"],
@@ -11,4 +12,22 @@ module.exports = {
   images: {
     domains: ["images.unsplash.com"],
   },
-}
+
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.(png|jpe?g|gif|svg)$/i,
+      use: [
+        {
+          loader: "file-loader",
+          options: {
+            name: "[name].[ext]",
+            publicPath: "/_next/static/images",
+            outputPath: "static/images",
+          },
+        },
+      ],
+    })
+
+    return config
+  },
+})
