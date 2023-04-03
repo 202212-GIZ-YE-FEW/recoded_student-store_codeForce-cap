326a290f2008d76dbc877760c6d36f1dede410c4
AASB7 <wwwwbasingab@gmail.com>
2023-04-03 20:10:56 +0530

    feat(input): create input component

--- src/components/input/Input.jsx
+++ src/components/input/Input.jsx
@@ -1,7 +1,18 @@
-import React from "react"
-
-function Input() {
-  return <div>input</div>
+function Input({ type, name, placeholder }) {
+  return (
+    <>
+      <input
+        type={type}
+        name={name}
+        placeholder={placeholder}
+        className=' my-2 block w-full rounded-lg
+    border border-gray-300  p-2.5 
+    text-center 
+  focus:ring-blue-500 focus:border-blue-500 '
+        required
+      />
+    </>
+  )
 }
 
 export default Input
