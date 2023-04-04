98d3c78fa77b0e589b4d3954073f1c6252c8e619
Abobakr Mahdi <93738112+BKR779@users.noreply.github.com>
2023-04-04 17:52:35 +0300

    fix(signup): fixing the tests error

--- src/components/SignUp/__test__/Signup.test.js
+++ src/components/SignUp/__test__/Signup.test.js
@@ -1,6 +1,6 @@
 import renderer from "react-test-renderer"
 
-import Signup from ".."
+import Signup from "../index"
 
 it("renders correctly", () => {
   const tree = renderer.create(<Signup />).toJSON()
