fda15c17b95463d577ecadb97b12ba2f79304309
Abobakr Mahdi <93738112+BKR779@users.noreply.github.com>
2023-04-04 18:00:37 +0300

    fix(signup): fixing the tests failer

--- src/components/SignUp/__test__/Signup.test.js
+++ src/components/SignUp/__test__/Signup.test.js
@@ -1,6 +1,6 @@
 import renderer from "react-test-renderer"
 
-import Signup from "../index"
+import Signup from ".."
 
 it("renders correctly", () => {
   const tree = renderer.create(<Signup />).toJSON()
