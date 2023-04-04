fabaf4de553024f36107fb0eac9bd6dc8c30c8f7
Abobakr Mahdi <93738112+BKR779@users.noreply.github.com>
2023-04-04 06:08:41 +0300

    refactor(highlighter): add the productListing highlighter style

--- src/components/highlighter/Highlighter.jsx
+++ src/components/highlighter/Highlighter.jsx
@@ -7,6 +7,7 @@ const HIGHLIGHTER = {
   category: `${img} ${text} ${block} bg-[length:70%_70%] text-[200%] md:text-[400%]`,
   priceFilter: `${img} ${text} lg:${block} hidden lg:bg-[length:70%_70%] text-[400%]`,
   singleProduct: `${img} ${text} ${block} lg:bg-[length:250.65px_38px] lg:text-[38px] md:bg-[length:197.35px_30px] md:text-[30px] bg-[length:140px_21px] text-[21px]`,
+  productListing: `${img} ${text} md:${block} hidden md:bg-[length:334.85px_30px] lg:bg-[length:437.27px_38px] md:text-[30px] lg:text-[38px]`,
 }
 
 export default function Highlighter({
