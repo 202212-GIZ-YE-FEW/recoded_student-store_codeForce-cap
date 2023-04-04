e8b7a74f1d463dca15b4c8158c8eb54d0532b7f3
Abobakr Mahdi <93738112+BKR779@users.noreply.github.com>
2023-04-04 06:18:05 +0300

    refactor(highlighter): add the ordersPage highlighter style

--- src/components/highlighter/Highlighter.jsx
+++ src/components/highlighter/Highlighter.jsx
@@ -9,7 +9,8 @@ const HIGHLIGHTER = {
   singleProduct: `${img} ${text} ${block} lg:bg-[length:250.65px_38px] lg:text-[38px] md:bg-[length:197.35px_30px] md:text-[30px] bg-[length:140px_21px] text-[21px]`,
   productListing: `${img} ${text} md:${block} hidden md:bg-[length:334.85px_30px] lg:bg-[length:437.27px_38px] md:text-[30px] lg:text-[38px]`,
   donate: `${img} ${text} ${block} lg:bg-[length:699.31px_65.38px] lg:text-[50px] md:bg-[length:400.2px_53.2px] md:text-[35px] bg-[length:298.4px_39.67px] text-[26px]`,
-  donating: `${img} ${text} ${block} lg:bg-[length:820.72px_76.43px] lg:text-[49px] md:bg-[length:571.49px_53.22px] md:text-[34.2499px] sm:bg-[length:339.1px_31.58px] sm:text-[20.3228px]`,
+  donating: `${img} ${text} ${block} lg:bg-[length:820.72px_76.43px] lg:text-[49px] md:bg-[length:571.49px_53.22px] md:text-[34.2499px] bg-[length:339.1px_31.58px] text-[20.3228px]`,
+  ordersPage: `${img} ${text} ${block} lg:bg-[length:437.27px_38px] lg:text-[38px] md:bg-[length:313.92px_27.28px] md:text-[27px] bg-[length:233.67px_20.31px] text-[21px]`,
 }
 
 export default function Highlighter({
