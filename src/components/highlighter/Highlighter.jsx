f5318d6c48511ff63d1e96f0d08ebc697337cab9
Abobakr Mahdi <93738112+BKR779@users.noreply.github.com>
2023-04-04 07:02:45 +0300

    revert(highlighter): fix the name of the size handle

--- src/components/highlighter/Highlighter.jsx
+++ src/components/highlighter/Highlighter.jsx
@@ -2,18 +2,18 @@
 const img = "bg-[url('/images/Highlighter.png')] bg-bottom bg-no-repeat"
 const text = "text-purple-light text-center font-bold tracking-[-1px]"
 const block = "block"
-const smSize = "lg:bg-[length:437.27px_38px] lg:text-[38px]"
-const lgSize = ""
+const lgSize = "lg:bg-[length:437.27px_38px] lg:text-[38px]"
+const mdSize = ""
 const HIGHLIGHTER = {
   category: `${img} ${text} ${block} bg-[length:70%_70%] text-[200%] md:text-[400%]`,
   priceFilter: `${img} ${text} lg:${block} hidden lg:bg-[length:70%_70%] text-[400%]`,
   singleProduct: `${img} ${text} ${block} lg:bg-[length:250.65px_38px] lg:text-[38px] md:bg-[length:197.35px_30px] md:text-[30px] bg-[length:140px_21px] text-[21px]`,
-  productListing: `${img} ${text} md:${block} ${smSize} md:bg-[length:334.85px_30px] md:text-[30px] hidden`,
+  productListing: `${img} ${text} md:${block} ${lgSize} md:bg-[length:334.85px_30px] md:text-[30px] hidden`,
   donate: `${img} ${text} ${block} lg:bg-[length:699.31px_65.38px] lg:text-[50px] md:bg-[length:400.2px_53.2px] md:text-[35px] bg-[length:298.4px_39.67px] text-[26px]`,
   donating: `${img} ${text} ${block} lg:bg-[length:820.72px_76.43px] lg:text-[49px] md:bg-[length:571.49px_53.22px] md:text-[34.2499px] bg-[length:339.1px_31.58px] text-[20.3228px]`,
-  ordersPage: `${img} ${text} ${block} ${smSize} md:bg-[length:313.92px_27.28px] md:text-[27px] bg-[length:233.67px_20.31px] text-[21px]`,
-  lestingPage: `${img} ${text} ${block} lg:bg-[length:200px_38px] lg:text-[38px] md:bg-[length:313.92px_27.28px] md:text-[27.28px] bg-[length:233.67px_20.31px] text-[20.31px]`,
-  editProfile: `${img} ${text} ${block} lg:bg-[length:200px_38px] lg:text-[30px] md:bg-[length:314px_28px] md:text-[28px] bg-[length:234px_20px] text-[20px]`,
+  ordersPage: `${img} ${text} ${block} ${lgSize} md:bg-[length:313.92px_27.28px] md:text-[27.28px] bg-[length:233.67px_20.31px] text-[21px]`,
+  lestingPage: `${img} ${text} ${block} lg:bg-[length:200px_38px] lg:text-[38px] md:bg-[length:313.92px_27.28px] md:text-[27.28px] bg-[length:233.67px_20.31px] text-[21px]`,
+  editProfile: `${img} ${text} ${block} lg:bg-[length:200px_38px] lg:text-[30px] md:bg-[length:313.92px_27.28px] md:text-[27.28px] bg-[length:233.67px_20.31px] text-[21px]`,
 }
 
 export default function Highlighter({
