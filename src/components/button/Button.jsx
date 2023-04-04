9a7debe5dee3489146e3c8c7feff16c64effcc9f
NabiL Ashbat <nbl.dev21@gmail.com>
2023-04-04 05:10:31 +0300

    docs: document my button component

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -1,5 +1,7 @@
+// Importing the PropTypes module from prop-types library
 import PropTypes from "prop-types"
 
+// Defining a constant BUTTON_STYLES object that contains different button styles
 const BUTTON_STYLES = {
   orangeSignIn:
     "h-10 w-40 lg:w-48 lg:px-5 p-1 px-4 rounded-lg border-1 bg-pumpkin text-md font-normal text-white shadow-sm",
@@ -17,6 +19,15 @@ const BUTTON_STYLES = {
     "h-12 w-56 lg:h-14 lg:w-64 m-1 p-1 px-2 hover:bg-purple hover:shadow-lg rounded-lg border-2 bg-pumpkin font-md text-17px text-white shadow-sm",
 }
 
+/**
+ * This is a React functional component that renders a button element with different styles.
+ * It accepts the following props:
+ * @param {string} buttonStyle - Specifies the button style to be applied. Defaults to "orangeSignIn".
+ * @param {string} type - Specifies the button type. Can be "button" or "submit". Defaults to "button".
+ * @param {string} text - Specifies the text content of the button. Required.
+ * @param {function} handleClick - Specifies the function to be executed on button click. Optional.
+ * @param {string} icon - Specifies the icon class name to be added to the button. Optional.
+ */
 function Button({
   buttonStyle = "orangeSignIn",
   type = "button",
@@ -36,6 +47,7 @@ function Button({
   )
 }
 
+// Defining propTypes for the Button component to specify the data types of its props
 Button.propTypes = {
   buttonStyle: PropTypes.oneOf(Object.keys(BUTTON_STYLES)),
   type: PropTypes.oneOf(["button", "submit"]),
@@ -44,4 +56,5 @@ Button.propTypes = {
   icon: PropTypes.string,
 }
 
+// Exporting the Button component as default
 export default Button
