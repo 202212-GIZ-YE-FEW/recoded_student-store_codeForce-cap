893901924eaae2af4b1ca9f7e4cad5e9e4801f10
Abobakr Mahdi <93738112+BKR779@users.noreply.github.com>
2023-04-04 06:12:41 +0300

    refactor(highlighter): add the donate highlighter style

--- src/components/highlighter/Highlighter.jsx
+++ src/components/highlighter/Highlighter.jsx
@@ -8,6 +8,7 @@ const HIGHLIGHTER = {
   priceFilter: `${img} ${text} lg:${block} hidden lg:bg-[length:70%_70%] text-[400%]`,
   singleProduct: `${img} ${text} ${block} lg:bg-[length:250.65px_38px] lg:text-[38px] md:bg-[length:197.35px_30px] md:text-[30px] bg-[length:140px_21px] text-[21px]`,
   productListing: `${img} ${text} md:${block} hidden md:bg-[length:334.85px_30px] lg:bg-[length:437.27px_38px] md:text-[30px] lg:text-[38px]`,
+  donate: `${img} ${text} ${block} lg:bg-[length:699.31px_65.38px] lg:text-[50px] md:bg-[length:400.2px_53.2px] md:text-[35px] bg-[length:298.4px_39.67px] text-[26px]`,
 }
 
 export default function Highlighter({
