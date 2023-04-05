66f61fcc546179774bc1f17b5f2cb12c47bff727
Abobakr Mahdi <93738112+BKR779@users.noreply.github.com>
2023-04-05 08:30:33 +0300

    refactor(sidebar): add the small screen style
    add the small screen style for the dynamic sidebar so that will be shown in the small screen
    
    #40

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -4,9 +4,9 @@ import Link from "next/link"
 import Highlighter from "../highlighter"
 
 const SIDE_BAR = {
-  ordersPage: "",
-  listingPage: "",
-  editProfile: "lg:w-[331px] lg:h-[947px] text-center lg:shadow-2xl lg:pt-12",
+  largeScreen:
+    "hidden lg:block lg:w-[331px] lg:h-[947px] text-center lg:shadow-2xl lg:pt-12",
+  smallScreen: "lg:hidden",
 }
 
 export default function SideBar({
@@ -14,33 +14,72 @@ export default function SideBar({
   name = "Name",
   email = "Email",
   location = "Location",
-  currentPageStyle = "editProfile",
+  currentPageStyle = "smallScreen",
 }) {
   return (
+    // <section className={SIDE_BAR[currentPageStyle]}>
+    //   <div className='flex flex-col justify-around h-[75%]'>
+    //     <Image
+    //       className='rounded-full mx-auto mb-3'
+    //       src={img}
+    //       alt='...'
+    //       width={171}
+    //       height={171}
+    //     />
+    //     <div className='text-[22px]'>
+    //       <h2 className='font-semibold'>{name}</h2>
+    //       <p>{email}</p>
+    //       <p>{location}</p>
+    //     </div>
+    //     <Link href={currentPageStyle}>
+    //       <Highlighter highlighterStyle='editProfile' text='Edit Profile' />
+    //     </Link>
+    //     <div className='font-bold text-[31px] grid grid-rows-2 gap-8'>
+    //       <h1>
+    //         <Link href={currentPageStyle}>My Listing</Link>
+    //       </h1>
+    //       <h1>
+    //         <Link href={currentPageStyle}>My Orders</Link>
+    //       </h1>
+    //     </div>
+    //   </div>
+    // </section>
+
     <section className={SIDE_BAR[currentPageStyle]}>
-      <div className='flex flex-col justify-around h-[75%]'>
-        <Image
-          className='rounded-full mx-auto mb-3'
-          src={img}
-          alt='...'
-          width={171}
-          height={171}
-        />
-        <div className='text-[22px]'>
-          <h2 className='font-semibold'>{name}</h2>
-          <p>{email}</p>
-          <p>{location}</p>
-        </div>
-        <Highlighter highlighterStyle='editProfile' text='Edit Profile' />
-        <div className='font-bold text-[31px] grid grid-rows-2 gap-8'>
-          <h1>
-            <Link href={currentPageStyle}>My Listing</Link>
-          </h1>
-          <h1>
-            <Link href={currentPageStyle}>My Orders</Link>
-          </h1>
-        </div>
+      <div className='grid grid-cols-3 text-[17px] items-center text-purple-dark text-center bg-[#90EEE1] h-[38px]'>
+        <Link
+          className='focus:bg-[#7874F2] focus:text-[#d7d7d7]'
+          href={currentPageStyle}
+        >
+          My Orders
+        </Link>
+        <Link
+          className='focus:bg-[#7874F2] focus:text-[#d7d7d7]'
+          href={currentPageStyle}
+        >
+          My Listing
+        </Link>
+        <Link
+          className='focus:bg-[#7874F2] focus:text-[#d7d7d7]'
+          href={currentPageStyle}
+        >
+          Edit Profile
+        </Link>
       </div>
+      <br />
+      <br />
+      <br />
+      <br />
+      <Highlighter highlighterStyle='editProfile' text='Edit Profile' />
+      <br />
+      <br />
+      <Image
+        className='rounded-full mx-auto'
+        src={img}
+        alt='...'
+        width={274}
+        height={275}
+      />
     </section>
   )
 }
