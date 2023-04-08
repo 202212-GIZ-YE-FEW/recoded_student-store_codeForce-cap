c3d0919ad0d7283abfba67c616034cac40d3fd67
AASB7 <wwwwbasingab@gmail.com>
2023-04-08 08:19:41 +0530

    feat(sign in): add icons for sign in

--- src/components/Signin/Signin.jsx
+++ src/components/Signin/Signin.jsx
@@ -1,4 +1,5 @@
 import Image from "next/image"
+import { BsFacebook, BsGoogle, BsTwitter } from "react-icons/bs"
 
 import styles from "./Signin.module.css"
 
@@ -35,8 +36,35 @@ function Signin() {
               placeholder='Surname'
               className='lg:w-96 md:w-64'
             />
-            <Button className='orangeSignIn' text='Sign in' />
+            <div className='my-4 gap-1 flex-row flex lg:justify-center'>
+              <Button className='orangeSignIn' text='Sign in' />
+              <Button className='forgotPassword' text='forgotPassword ' />
+            </div>
           </form>
+          <div className='flex items-center'>
+            <div className='my-1 mr-2 h-px mt-[10px] w-[164px] bg-[#9dafbd]'></div>
+            <p>Or</p>
+            <div className='my-1 mr-2 h-px mt-[10px] w-[164px] bg-[#9dafbd]'></div>
+          </div>
+          <p className='text-md m-1 text-[#647581]'>Sign-up-with</p>
+          <div className='m-1 mb-8 flex flex-row  '>
+            <button className=' m-1 flex items-center rounded-3xl border border-[#F26F6F] p-1  text-[#F26F6F]'>
+              <BsGoogle color='#F26F6F' size={24} style={{ padding: "1px" }} />
+              <p className='mx-2 text-sm md:mx-3'>Google</p>
+            </button>
+            <button className='color-darkPurple m-1  flex items-center rounded-3xl border border-[#485DCF] p-1 text-[#485DCF]'>
+              <BsFacebook
+                color='#485DCF'
+                size={24}
+                style={{ padding: "1px" }}
+              />
+              <p className='mx-2 text-sm md:mx-3'>Facebook</p>
+            </button>
+            <button className=' m-1 flex justify-around rounded-3xl border border-[#28C7FA]  p-1 text-[#28C7FA] '>
+              <BsTwitter color='#28C7FA' size={24} style={{ padding: "1px" }} />
+              <p className='mx-2 text-sm md:mx-3'>Twitter</p>
+            </button>
+          </div>
         </div>
       </div>
     </>
