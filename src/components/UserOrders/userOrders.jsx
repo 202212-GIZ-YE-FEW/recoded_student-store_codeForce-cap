115fea95fc0f2e8a46f569c6ca55f3ef0f6cbe7e
Abobakr Mahdi <93738112+BKR779@users.noreply.github.com>
2023-04-10 02:49:07 +0300

    style(user order): add the first style for the user order component

--- src/components/UserOrders/userOrders.jsx
+++ src/components/UserOrders/userOrders.jsx
@@ -1,9 +1,25 @@
+import Image from "next/image"
+
+import products from "../ProductList/products"
 import SideBar from "../SideBar"
+
+const gradient = "lg:bg-gradient-to-l from-zinc-800 to-slate-300"
 
 export default function userOrder() {
   return (
-    <section>
-      <SideBar currentPage='ordersPage' highlighterText='My Orders' />
+    <section className='flex flex-col lg:flex-row'>
+      <div>
+        <SideBar currentPage='ordersPage' highlighterText='My Orders' />
+      </div>
+      <div className={`${gradient} lg:pt-16 h-[947px] overflow-y-auto block`}>
+        <div className='flex flex-wrap gap-5 lg:pl-16'>
+          {products.map((product) => (
+            <div key={product.id} className=''>
+              <Image src='/productImg.png' alt='...' height={295} width={242} />
+            </div>
+          ))}
+        </div>
+      </div>
     </section>
   )
 }
