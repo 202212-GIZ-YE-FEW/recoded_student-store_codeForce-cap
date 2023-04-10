a9e22aa631799fdb5d56e5eda2980cc1e88b3404
Abobakr Mahdi <93738112+BKR779@users.noreply.github.com>
2023-04-10 05:22:52 +0300

    test(user order): last try fix the user order test before delet it

--- src/components/UserOrders/__test__/UserOrders.test.jsx
+++ src/components/UserOrders/__test__/UserOrders.test.jsx
@@ -1,6 +1,6 @@
 import renderer from "react-test-renderer"
 
-import UserOrders from "../UserOrders"
+import UserOrders from ".."
 
 it("renders correctly", () => {
   const tree = renderer.create(<UserOrders />).toJSON()
