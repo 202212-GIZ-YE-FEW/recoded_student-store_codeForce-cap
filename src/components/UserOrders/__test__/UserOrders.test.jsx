dd7fc44e279e250b15a802b75579e3b8d6744207
Abobakr Mahdi <93738112+BKR779@users.noreply.github.com>
2023-04-10 06:30:36 +0300

    test(user order): remove the user order test until another time

--- src/components/UserOrders/__test__/UserOrders.test.jsx
+++
@@ -1,9 +0,0 @@
-import renderer from "react-test-renderer"
-
-import UserOrders from ".."
-
-test("UserOrders component renders correctly", () => {
-  const component = renderer.create(<UserOrders />)
-  let tree = component.toJSON()
-  expect(tree).toMatchSnapshot()
-})
