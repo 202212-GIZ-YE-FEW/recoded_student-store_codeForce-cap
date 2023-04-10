79046fd90cf503b9a57bc2d1cc3b1b0fdb1b545d
Abobakr Mahdi <93738112+BKR779@users.noreply.github.com>
2023-04-10 05:15:38 +0300

    test(user order): another try to pass the test

--- src/components/UserOrders/__test__/UserOrders.test.jsx
+++ src/components/UserOrders/__test__/UserOrders.test.jsx
@@ -1,6 +1,6 @@
 import renderer from "react-test-renderer"
 
-import UserOrders from "../index"
+import UserOrders from "../UserOrders"
 
 it("renders correctly", () => {
   const tree = renderer.create(<UserOrders />).toJSON()
