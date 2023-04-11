e8a1f1cfdc2ad65f85e2aad6c06ced5e15ba3509
messam10 <memaq1998.10.7@gmail.com>
2023-04-11 20:42:57 +0300

    feat(cardview): add dynamic list to icons

--- src/components/CardView/CardView.jsx
+++ src/components/CardView/CardView.jsx
@@ -1,6 +1,22 @@
 import Image from "next/image"
 
+function ImageList(personalURL, imageName, altName) {
+  return (
+    <li>
+      <a href={personalURL} className='text-gray-500 hover:text-gray-900'>
+        <Image src={imageName} width={35} height={35} alt={altName} />
+      </a>
+    </li>
+  )
+}
+
 function CardView() {
+  const images = {
+    facebook: "facebook-circle.svg",
+    twitter: "twitter-circle.svg",
+    github: "github.svg",
+  }
+
   return (
     <div className='items-center bg-gray-50 rounded-lg shadow sm:flex'>
       <a href='#'>
@@ -23,45 +39,9 @@ function CardView() {
         <p className='mt-3 mb-4 font-semibold text-gray-500'>describe</p>
 
         <ul className='flex space-x-4 sm:mt-0'>
-          <li>
-            <a
-              href='#'
-              className='text-gray-500 hover:text-gray-900 dark:hover:text-white'
-            >
-              <Image
-                src='facebook-circle.svg'
-                width={60}
-                height={60}
-                alt='Facebook'
-              />
-            </a>
-          </li>
-          <li>
-            <a
-              href='#'
-              className='text-gray-500 hover:text-gray-900 dark:hover:text-white'
-            >
-              <Image
-                src='twitter-circle.svg'
-                width={60}
-                height={60}
-                alt='twitter logo'
-              />
-            </a>
-          </li>
-          <li>
-            <a
-              href='#'
-              className='text-gray-500 hover:text-gray-900 dark:hover:text-white'
-            >
-              <Image
-                src='github.svg'
-                width={60}
-                height={60}
-                alt='github logo'
-              />
-            </a>
-          </li>
+          {ImageList("#", images.facebook, "facebook")}
+          {ImageList("#", images.twitter, "twitter")}
+          {ImageList("#", images.github, "github")}
         </ul>
       </div>
     </div>
