4fa8f2ae8190a684d231bf88f254f9b7a9c12221
Abobakr Mahdi <93738112+BKR779@users.noreply.github.com>
2023-04-12 20:38:05 +0300

    refactor(edit profile): add the required field to the necessary things

--- src/components/EditProfile/EditProfile.jsx
+++ src/components/EditProfile/EditProfile.jsx
@@ -4,11 +4,11 @@ import Input from "../input"
 export default function EditProfile() {
   return (
     <>
-      <Input placeholder='Name' type='text' />
+      <Input placeholder='Name' type='text' required />
       <Input placeholder='Surname' type='text' />
-      <Input placeholder='Email' type='email' />
+      <Input placeholder='Email' type='email' required />
       <Input placeholder='Phone number' type='number' />
-      <Input placeholder='New Password' type='password' />
+      <Input placeholder='New Password' type='password' required />
       <Input placeholder='Confirm Nwe Password' type='password' />
       <Button buttonStyle='saveChanges' text='Save Changes' />
     </>
