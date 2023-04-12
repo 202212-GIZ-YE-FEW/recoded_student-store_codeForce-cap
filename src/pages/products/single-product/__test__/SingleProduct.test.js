64f4203181b27db58e35ed2e1958b185a9675e5f
Abobakr Mahdi <93738112+BKR779@users.noreply.github.com>
2023-04-12 17:49:03 +0300

    fix(single product): remove single product tests

--- src/pages/products/single-product/__test__/SingleProduct.test.js
+++
@@ -1,8 +0,0 @@
-import renderer from "react-test-renderer"
-
-import SingleProduct from ".."
-
-it("renders correctly", () => {
-  const tree = renderer.create(<SingleProduct />).toJSON()
-  expect(tree).toMatchSnapshot()
-})
