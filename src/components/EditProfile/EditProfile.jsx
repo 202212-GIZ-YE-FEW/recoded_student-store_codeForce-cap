f7112a44c10dfe19d9e13a3171c92f95e8efb176
Abobakr Mahdi <93738112+BKR779@users.noreply.github.com>
2023-04-12 20:21:06 +0300

    refactor(edit profile): add the inputs type for the inputs method

--- src/components/EditProfile/EditProfile.jsx
+++ src/components/EditProfile/EditProfile.jsx
@@ -4,12 +4,12 @@ import Input from "../input"
 export default function EditProfile() {
   return (
     <>
-      <Input placeholder='Name' />
-      <Input placeholder='Surname' />
-      <Input placeholder='Email' />
-      <Input placeholder='Phone number' />
-      <Input placeholder='New Password' />
-      <Input placeholder='Confirm Nwe Password' />
+      <Input placeholder='Name' type='text' />
+      <Input placeholder='Surname' type='text' />
+      <Input placeholder='Email' type='email' />
+      <Input placeholder='Phone number' type='number' />
+      <Input placeholder='New Password' type='password' />
+      <Input placeholder='Confirm Nwe Password' type='password' />
       <Button buttonStyle='saveChanges' text='Save Changes' />
     </>
   )
