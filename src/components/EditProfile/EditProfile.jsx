2ce116e28a5bc223a79a4e8c7b4b3d718d2cc3f7
Abobakr Mahdi <93738112+BKR779@users.noreply.github.com>
2023-04-12 19:41:23 +0300

    refactor(edit profile): add the Edit Profile inputs
    #21

--- src/components/EditProfile/EditProfile.jsx
+++ src/components/EditProfile/EditProfile.jsx
@@ -1,3 +1,16 @@
+import Button from "../button"
+import Input from "../input"
+
 export default function EditProfile() {
-  return <div>EditProfile</div>
+  return (
+    <>
+      <Input placeholder='Name' />
+      <Input placeholder='Surname' />
+      <Input placeholder='Email' />
+      <Input placeholder='Phone number' />
+      <Input placeholder='New Password' />
+      <Input placeholder='Confirm Nwe Password' />
+      <Button buttonStyle='saveChanges' text='Save Changes' />
+    </>
+  )
 }
