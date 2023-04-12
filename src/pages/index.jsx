26aa64d080a775e3fb013cf50740a5400da2a36d
Abobakr Mahdi <93738112+BKR779@users.noreply.github.com>
2023-04-12 21:55:43 +0300

    docs(index): run the lint fix to sort the imports

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,6 +1,6 @@
+import Link from "next/link"
 import { useTranslation } from "next-i18next"
 import { serverSideTranslations } from "next-i18next/serverSideTranslations"
-import Link from "next/link"
 
 import Aboutus from "@/components/aboutus"
 import Donation from "@/components/donation"
