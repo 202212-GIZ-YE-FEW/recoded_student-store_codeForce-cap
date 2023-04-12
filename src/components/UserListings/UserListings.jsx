4766b9a5724ba43374db313213d6ac0a280e0f8c
Abobakr Mahdi <93738112+BKR779@users.noreply.github.com>
2023-04-12 06:24:48 +0300

    revert(user listings): add the userListings style and the products in it
    add userListings products whitch will be displayed in the screen when the user click in it's link in
    the side bar
    
    #20

--- src/components/UserListings/UserListings.jsx
+++ src/components/UserListings/UserListings.jsx
@@ -1,3 +1,59 @@
+import Image from "next/image"
+
+import products from "../ProductList/products"
+
 export default function UserListings() {
-  return <div>UserListings</div>
+  return (
+    <section className='flex flex-col lg:flex-row'>
+      <div className='lg:bg-gradient-to-l from-zinc-800 to-slate-300 lg:py-16 h-[947px] overflow-y-auto w-full'>
+        <div className='grid lg:grid-cols-3 md:grid-cols-2 gap-5 md:px-20 px-8'>
+          {products.map((product) => (
+            <div
+              key={product.id}
+              className='lg:bg-white rounded-lg pb-7 shadow-2xl'
+            >
+              <Image
+                className='rounded-t-lg shadow-lg w-full'
+                src='/productImg.png'
+                alt='...'
+                width={1920}
+                height={1080}
+              />
+              <div className='mx-2'>
+                <div className='flex justify-between my-4'>
+                  <div className='text-left'>
+                    <h2 className='font-semibold'>{product.name}</h2>
+                    <p className='font-extralight text-xs'>
+                      {product.category}
+                    </p>
+                  </div>
+                  <div>
+                    <h2 className='font-extrabold text-xl text-right'>
+                      ${product.price}
+                    </h2>
+                    <p className='font-extralight text-xs'>
+                      {product.made_city}
+                    </p>
+                  </div>
+                </div>
+                <div>
+                  <p>
+                    <span className='font-extrabold'>Listing Date:</span>{" "}
+                    {product.order_date}
+                  </p>
+                  <p>
+                    <span className='font-extrabold'>Category:</span>
+                    <br />
+                    <span className='font-extralight text-xs'>
+                      {product.category}
+                    </span>
+                  </p>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  )
 }
