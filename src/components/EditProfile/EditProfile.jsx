12cc8658f6bd7bf9693a5b0e8bcb1a8619df4b76
Abobakr Mahdi <93738112+BKR779@users.noreply.github.com>
2023-04-12 21:40:16 +0300

    refactor(edit profile): add the EditProfile form submitting

--- src/components/EditProfile/EditProfile.jsx
+++ src/components/EditProfile/EditProfile.jsx
@@ -2,15 +2,20 @@ import Button from "../button"
 import Input from "../input"
 
 export default function EditProfile() {
+  const handleSubmit = (event) => {
+    event.preventDefault() // Prevent form from submitting
+    alert("Form submitted")
+  }
+
   return (
-    <>
+    <form onSubmit={handleSubmit}>
       <Input placeholder='Name' type='text' required />
       <Input placeholder='Surname' type='text' />
       <Input placeholder='Email' type='email' required />
       <Input placeholder='Phone number' type='number' />
       <Input placeholder='New Password' type='password' required />
       <Input placeholder='Confirm Nwe Password' type='password' />
-      <Button buttonStyle='saveChanges' text='Save Changes' />
-    </>
+      <Button buttonStyle='saveChanges' text='Save Changes' type='submit' />
+    </form>
   )
 }
