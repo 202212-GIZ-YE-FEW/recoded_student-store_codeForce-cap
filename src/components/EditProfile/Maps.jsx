9a9d3b61ec473c21897d8272abf890fdc18dd3f5
Abobakr Mahdi <93738112+BKR779@users.noreply.github.com>
2023-04-13 18:27:06 +0300

    revert(edit profile): change maps function from MapAddress to Maps to make it more readable

--- src/components/EditProfile/Maps.jsx
+++ src/components/EditProfile/Maps.jsx
@@ -1,6 +1,6 @@
 import { MapContainer, Marker, TileLayer } from "react-leaflet"
 
-export default function MapAddress() {
+export default function Maps() {
   return (
     <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
       <TileLayer
