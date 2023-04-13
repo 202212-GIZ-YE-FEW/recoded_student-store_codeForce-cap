1db4fb321b94f70a2c748162ca9ec1f4048d7308
AASB7 <wwwwbasingab@gmail.com>
2023-04-14 02:29:43 +0530

    test(user otder): remove userorder test files

--- src/components/UserOrders/__test__/UserOrders.test.js
+++
@@ -1,8 +0,0 @@
-import renderer from "react-test-renderer"
-
-import UserOrders from ".."
-
-it("renders correctly", () => {
-  const tree = renderer.create(<UserOrders />).toJSON()
-  expect(tree).toMatchSnapshot()
-})
