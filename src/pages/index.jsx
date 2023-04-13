3fabd77b6824de4ec067ed259d4981401a208ab8
AASB7 <wwwwbasingab@gmail.com>
2023-04-13 07:41:41 +0530

    fix(userorders): fix the import for userorders in index-pages

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -24,7 +24,7 @@ export default function HomePage() {
         <Navbar />
         <OurTeam />
         <Aboutus />
-        <UserOrders />
+        <userOrders />
         <SingleProduct />
         <Signup />
         {/* <Signin /> */}
