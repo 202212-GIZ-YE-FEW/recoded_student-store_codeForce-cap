ac647bed19279891c38802d262d5671a5755216d
Abobakr Mahdi <93738112+BKR779@users.noreply.github.com>
2023-04-13 18:34:21 +0300

    refactor(edit profile): import maps style to fix the maps displaying issues

--- src/components/EditProfile/Maps.jsx
+++ src/components/EditProfile/Maps.jsx
@@ -1,5 +1,7 @@
 import { MapContainer, Marker, TileLayer } from "react-leaflet"
 
+import "leaflet/dist/leaflet.css"
+
 export default function Maps() {
   return (
     <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
