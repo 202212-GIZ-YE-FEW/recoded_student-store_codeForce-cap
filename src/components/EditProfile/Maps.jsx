edaf2fc124ea4e42c8c840de425c8061decba7f6
Abobakr Mahdi <93738112+BKR779@users.noreply.github.com>
2023-04-13 18:36:26 +0300

    refactor(edit profile): add maps width and height to display it correctly in the edit profile

--- src/components/EditProfile/Maps.jsx
+++ src/components/EditProfile/Maps.jsx
@@ -4,7 +4,12 @@ import "leaflet/dist/leaflet.css"
 
 export default function Maps() {
   return (
-    <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+    <MapContainer
+      center={[51.505, -0.09]}
+      zoom={13}
+      scrollWheelZoom={false}
+      style={{ width: "250px", height: "250px" }}
+    >
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
