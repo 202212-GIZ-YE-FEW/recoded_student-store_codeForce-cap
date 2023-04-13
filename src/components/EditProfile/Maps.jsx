cb9fb07e05674eaf056f1c20b4d3f27e063a674c
Abobakr Mahdi <93738112+BKR779@users.noreply.github.com>
2023-04-13 19:23:15 +0300

    refactor(edit profile): remove the maps marker

--- src/components/EditProfile/Maps.jsx
+++ src/components/EditProfile/Maps.jsx
@@ -1,4 +1,4 @@
-import { MapContainer, Marker, TileLayer } from "react-leaflet"
+import { MapContainer, TileLayer } from "react-leaflet"
 
 import "leaflet/dist/leaflet.css"
 
@@ -14,7 +14,6 @@ export default function Maps() {
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
       />
-      <Marker position={[51.505, -0.09]} />
     </MapContainer>
   )
 }
