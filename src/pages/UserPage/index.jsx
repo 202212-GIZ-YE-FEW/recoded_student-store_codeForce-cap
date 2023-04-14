8439a2a5ffca5cefef026f38e5e537c1ce903fc0
Abobakr Mahdi <93738112+BKR779@users.noreply.github.com>
2023-04-15 01:53:48 +0300

    refactor(userpage): add the UserPage returnd components
    add the UserPage returned components that will be displaying dependes on the clicked link
    
    #27

--- src/pages/UserPage/index.jsx
+++ src/pages/UserPage/index.jsx
@@ -1,9 +1,21 @@
 import { useState } from "react"
 
+import EditProfile from "@/components/EditProfile"
+import SideBar from "@/components/SideBar"
+import UserListings from "@/components/UserListings"
+import UserOrders from "@/components/UserOrders"
+
 export default function UserPage() {
   const [selectedPage, setSelectedPage] = useState("UserOrders")
   function handleSelectedPage(page) {
     setSelectedPage(page)
   }
-  return <div>UserPage</div>
+  return (
+    <div className='flex'>
+      <SideBar handleSelectedPage={handleSelectedPage} />
+      {selectedPage === "EditProfile" && <EditProfile />}
+      {selectedPage === "UserListings" && <UserListings />}
+      {selectedPage === "UserOrders" && <UserOrders />}
+    </div>
+  )
 }
