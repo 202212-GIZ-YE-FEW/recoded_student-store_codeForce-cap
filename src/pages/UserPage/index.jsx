3828c64b45b4d0eca34f15c51a96b6defe4b4cb0
Abobakr Mahdi <93738112+BKR779@users.noreply.github.com>
2023-04-15 01:49:40 +0300

    refactor(userpage): add the selected page handler
    add the selectedPageHandler that will handle the displayed component
    
    #27

--- src/pages/UserPage/index.jsx
+++ src/pages/UserPage/index.jsx
@@ -1,3 +1,9 @@
+import { useState } from "react"
+
 export default function UserPage() {
+  const [selectedPage, setSelectedPage] = useState("UserOrders")
+  function handleSelectedPage(page) {
+    setSelectedPage(page)
+  }
   return <div>UserPage</div>
 }
