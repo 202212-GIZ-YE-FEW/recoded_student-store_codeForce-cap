117318bcafa08d3dc87760d9b29bb1c18785eafd
Abobakr Mahdi <93738112+BKR779@users.noreply.github.com>
2023-04-15 08:48:04 +0300

    style(highlighter): update some style issues

--- src/components/highlighter/Highlighter.jsx
+++ src/components/highlighter/Highlighter.jsx
@@ -2,7 +2,7 @@
 const img = "bg-[url('/images/Highlighter.png')] bg-bottom bg-no-repeat"
 const textStyle =
   "text-purple-light font-poppins text-center font-bold tracking-[-1px]"
-const lgSize = "lg:bg-[length:437.27px_38px] lg:text-[38px]"
+const lgSize = "lg:bg-[length:290px_38px] lg:text-[38px]"
 const mdSize = "md:bg-[length:313.92px_27.28px] md:text-[27.28px]"
 const smSize = "bg-[length:233.67px_20.31px] text-[21px]"
 
@@ -15,8 +15,8 @@ const HIGHLIGHTER = {
   donate: `${img} block lg:bg-[length:699.31px_65.38px] lg:text-[50px] md:bg-[length:400.2px_53.2px] md:text-[35px] bg-[length:298.4px_39.67px] text-[26px]`,
   donating: `${img} block lg:bg-[length:820.72px_76.43px] lg:text-[49px] md:bg-[length:571.49px_53.22px] md:text-[34.2499px] bg-[length:339.1px_31.58px] text-[21px]`,
   ordersPage: `${img} block ${lgSize} ${mdSize} ${smSize}`,
-  listingPage: `${img} block ${mdSize} ${smSize} lg:bg-[length:200px_38px] lg:text-[38px]`,
-  editProfile: `${img} block ${mdSize} ${smSize} lg:bg-[length:200px_38px] lg:text-[30px]`,
+  listingPage: `${img} block ${lgSize} ${mdSize} ${smSize} lg:text-[38px]`,
+  editProfile: `${img} block ${mdSize} ${smSize} lg:bg-[length:240px_38px] lg:text-[30px]`,
   aboutus: `${img} block ${mdSize} ${smSize} bg-[length:400px_80px] text-[55px] lg:bg-[length:400px_80px] lg:text-[55px]`,
 }
 
