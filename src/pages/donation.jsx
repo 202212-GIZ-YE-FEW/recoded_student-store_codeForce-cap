006dd35d6a0a47daba7b04ed9afe8f1ca12f918e
messam10 <memaq1998.10.7@gmail.com>
2023-04-17 00:23:00 +0300

    feat(donation): create donation page

---
+++ src/pages/donation.jsx
@@ -0,0 +1,13 @@
+import Donation from "@/components/donation"
+
+import RootLayout from "@/layout/root/RootLayout"
+
+function OurTeamPage() {
+  return (
+    <RootLayout>
+      <Donation />
+    </RootLayout>
+  )
+}
+
+export default OurTeamPage
