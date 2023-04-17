1174e60f6187dcd99cf3649d3c7b62d0f7b15a76
Abobakr Mahdi <93738112+BKR779@users.noreply.github.com>
2023-04-17 04:25:20 +0300

    refactor(navbar): change the sing up to be sign in

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -119,9 +119,9 @@ export default function Navbar() {
         {/* ----------- Buttons ----------- */}
         <div className='flex  justify-between items-center gap-5 flex-col md:flex-row '>
           <div className='flex gap-3 capitalize flex-col md:flex-row'>
-            <Link href='/signup'>
+            <Link href='/signin'>
               <div className='bg-purple-light py-2 px-5 text-white rounded-3xl text-sm hover:bg-violet-800 transition-all cursor-pointer'>
-                sign up
+                sign in
               </div>
             </Link>
             <Link href='/sell-items'>
