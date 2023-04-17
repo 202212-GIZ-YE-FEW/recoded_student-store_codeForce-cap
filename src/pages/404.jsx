355b02b6441f23e8534de9657b57f2b325ee3821
messam10 <memaq1998.10.7@gmail.com>
2023-04-18 01:40:29 +0300

    feat(404-page): add page base

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,5 +1,11 @@
 import RootLayout from "@/layout/root/RootLayout"
 
 export default function NotFoundPage() {
-  return <RootLayout>404</RootLayout>
+  return (
+    <RootLayout>
+      <div className='container flex flex-col items-center justify-center px-5 mx-auto my-8'>
+        <div className='max-w-md text-center'></div>
+      </div>
+    </RootLayout>
+  )
 }
