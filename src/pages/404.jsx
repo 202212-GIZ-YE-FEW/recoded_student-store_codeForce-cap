5dce87534d19bd12c32b02e423cbd2072c3e31c6
messam10 <memaq1998.10.7@gmail.com>
2023-04-18 01:46:06 +0300

    feat(404-page): add error 404 text

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -4,7 +4,11 @@ export default function NotFoundPage() {
   return (
     <RootLayout>
       <div className='container flex flex-col items-center justify-center px-5 mx-auto my-8'>
-        <div className='max-w-md text-center'></div>
+        <div className='max-w-md text-center'>
+          <h2 className='mb-8 font-extrabold text-9xl'>
+            <span className='sr-only'>Error</span>404
+          </h2>
+        </div>
       </div>
     </RootLayout>
   )
