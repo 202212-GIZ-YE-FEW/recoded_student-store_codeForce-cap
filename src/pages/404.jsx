4a3f4fb4cd0145acb23b3fa2e7ad280666f89e92
messam10 <memaq1998.10.7@gmail.com>
2023-04-18 02:25:04 +0300

    style(404-page): text and size improvements

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -7,18 +7,19 @@ export default function NotFoundPage() {
       <div className='container flex flex-col items-center justify-center px-5 mx-auto my-8'>
         <div className='max-w-md text-center'>
           <h2 className='mb-8 font-extrabold text-9xl text-purple'>404</h2>
-          <p className='text-2xl font-semibold md:text-3xl'>
-            We could not find the page you are looking for.
-          </p>
+          <p className='text-2xl font-bold'>We could not find the page.</p>
           <p className='mt-4 mb-8 font-semibold'>
-            But dont worry, you can find plenty of other things on our homepage.
+            Do not worry, you can find plenty of other things at home.
           </p>
           <Link
             href='/'
             className='px-8 py-3 font-bold rounded bg-orange-500 text-white'
           >
-            Back to homepage
+            Back to Home
           </Link>
+          <br />
+          <br />
+          <br />
         </div>
       </div>
     </RootLayout>
