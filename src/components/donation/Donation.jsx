7ea1afe0737afefd069786c98e2b2bd42c812f80
messam10 <memaq1998.10.7@gmail.com>
2023-04-22 00:33:43 +0300

    refactor(donation): translation of the donation component

--- src/components/donation/Donation.jsx
+++ src/components/donation/Donation.jsx
@@ -28,26 +28,17 @@ function Donation({ t }) {
         ></Image>
       </div>
       <div className='flex flex-col mt-20 items-center justify-center max-w-screen-md'>
-        <Highlighter highlighterClassName='donate' text='Why Donate?' />
+        <Highlighter highlighterClassName='donate' text={t("donate")} />
         <p className='text-purple-dark p-3 text-4xl leading-relaxed text-center'>
-          Life is busy, and it can sometimes be easy to forget to show your
-          gratitude for all that you’ve been given. Students have low budgets
-          and they’re in need of financial support. When you are ready to give
-          and are researching groups of people to support, this can remind us of
-          all that we have, and the act of donating to charity is a way to
-          express our feelings gratitude. Inspire others to give by posting your
-          kind action on social media to inspire others to give generously.
+          {t("donate-des")}
         </p>
       </div>
-      <Highlighter
-        highlighterClassName='donating'
-        text='Make a Difference by Donating!'
-      />
+      <Highlighter highlighterClassName='donating' text={t("make-donate")} />
       {/* Cards section */}
       <div className='flex flex-row flex-wrap items-center justify-around gap-12 p-10'>
         <div className='mt-20 h-[240px] w-[280px] flex flex-col  justify-center items-center rounded-xl bg-white text-purple-almostblack shadow-lg'>
           <div className='mt-8 flex flex-col items-center gap-2'>
-            <span className='text-xl'>Small Help</span>
+            <span className='text-xl'>{t("small-help")}</span>
             <div className='relative flex items-center'>
               {/* Displays currency icon */}
               <span className='absolute text-2xl -left-3 top-6'>
@@ -62,12 +53,12 @@ function Donation({ t }) {
             </div>
           </div>
           <button className='flex h-12 w-[65%] items-center justify-center rounded bg-pumpkin px-14 text-sm text-white  transition-all duration-[0.3s] ease-[ease-in-out] cursor-pointer hover:bg-purple'>
-            Donate
+            {t("donate-button")}
           </button>
         </div>
         <div className='mt-10 h-[280px] w-[320px] flex flex-col  justify-center items-center rounded-xl bg-white text-purple-almostblack shadow-lg'>
           <div className='mt-6 flex flex-col items-center'>
-            <span className='mb-4 text-2xl'>Some Help</span>
+            <span className='mb-4 text-2xl'>{t("some-help")}</span>
             <div className='relative flex items-center'>
               <span className='absolute text-2xl -left-3 top-8'>
                 <Image
@@ -81,11 +72,11 @@ function Donation({ t }) {
             </div>
           </div>
           <button className='flex h-12 w-[65%] items-center justify-center rounded bg-pumpkin px-[70px] text-sm  text-white transition-all duration-[0.3s] ease-[ease-in-out] cursor-pointer hover:bg-purple'>
-            Donate
+            {t("donate-button")}
           </button>
         </div>
         <div className='h-[320px] w-[380px] flex flex-col  justify-center items-center rounded-xl bg-white text-purple-almostblack shadow-lg'>
-          <span className='text-3xl'>Big Help</span>
+          <span className='text-3xl'>{t("big-help")}</span>
           <div className='relative flex items-center'>
             <span className='absolute text-3xl -left-4 top-18'>
               <Image
@@ -98,7 +89,7 @@ function Donation({ t }) {
             <span className='text-[69px] font-bold'>50</span>
           </div>
           <button className='flex h-12 w-[65%] items-center justify-center rounded bg-pumpkin px-20 text-xl text-white transition-all duration-[0.3s] ease-[ease-in-out] cursor-pointer hover:bg-purple'>
-            Donate!
+            {t("donate-button")}
           </button>
         </div>
       </div>
