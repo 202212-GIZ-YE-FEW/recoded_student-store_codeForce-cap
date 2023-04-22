a54106d5204d328017426df83e0790b119775168
Abobakr Mahdi <93738112+BKR779@users.noreply.github.com>
2023-04-22 20:29:56 +0300

    fix: signin Links import

--- src/components/Signin/Signin.jsx
+++ src/components/Signin/Signin.jsx
@@ -1,4 +1,5 @@
 import Image from "next/image"
+import Link from "next/link"
 import { useRouter } from "next/router"
 import { useState } from "react"
 import { BsFacebook, BsGoogle, BsTwitter } from "react-icons/bs"
