97995bfbb2ccfe1f6b751ce4e0c6670151939707
Abobakr Mahdi <93738112+BKR779@users.noreply.github.com>
2023-04-24 06:16:17 +0300

    refactor: add the list type selector

--- src/components/Sellitems/Sellitems.jsx
+++ src/components/Sellitems/Sellitems.jsx
@@ -68,6 +68,18 @@ export default function Sellitems() {
           accept='image/*'
         />
       </label>
+
+      <label for='typeSelector' className='sr-only'>
+        Underline select
+      </label>
+      <select
+        id='typeSelector'
+        className='cursor-pointer block py-2.5 px-0 w-full text-sm text-gray-500 bg-transparent border-0 border-b-2 border-gray-200 appearance-none dark:text-gray-400 dark:border-gray-700 focus:outline-none focus:ring-0'
+      >
+        <option selected>Choose a type</option>
+        <option value='product'>Product</option>
+        <option value='service'>Service</option>
+      </select>
     </div>
   )
 }
