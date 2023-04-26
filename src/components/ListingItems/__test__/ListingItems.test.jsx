fb00381f8e856bda87fe8191e9086df7dd54546a
Abobakr Mahdi <93738112+BKR779@users.noreply.github.com>
2023-04-26 03:40:28 +0300

    test(listing items): remove the test to another time

--- src/components/ListingItems/__test__/ListingItems.test.jsx
+++
@@ -1,8 +0,0 @@
-import renderer from "react-test-renderer"
-
-import ListingItems from "../ListingItems"
-
-it("renders correctly", () => {
-  const tree = renderer.create(<ListingItems />).toJSON()
-  expect(tree).toMatchSnapshot()
-})
