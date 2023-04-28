ef4a16abf638ef14db6a6a8e23061e753c3f3eb6
Abobakr Mahdi <93738112+BKR779@users.noreply.github.com>
2023-04-29 01:23:48 +0300

    feat(listingItems): add the schema validation of the listingItems page

---
+++ src/utils/schemaValidations/listingItems.jsx
@@ -0,0 +1,24 @@
+import * as Yup from "yup"
+
+export const listingsValidation = Yup.object().shape({
+  // primaryImage: Yup.object().shape({
+  //   file: Yup.mixed().optional(),
+  //   url: Yup.string().url().optional(),
+  // }),
+  // secondaryImage: Yup.object().shape({
+  //   file: Yup.mixed().optional(),
+  //   url: Yup.string().url().optional(),
+  // }),
+  // tertiaryImage: Yup.object().shape({
+  //   file: Yup.mixed().optional(),
+  //   url: Yup.string().url().optional(),
+  // }),
+  type: Yup.string().required("Type is required"),
+  category: Yup.string().required("Category is required"),
+  productName: Yup.string().required("Product name is required"),
+  description: Yup.string().required("Description is required"),
+  location: Yup.string().required("Location is required"),
+  price: Yup.number()
+    .required("Price is required")
+    .min(0, "Price must be greater than or equal to 0"),
+})
