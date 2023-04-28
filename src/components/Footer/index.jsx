ff5c6331e5c02f12d719b89b52cd1d60e3cc3407
Abobakr Mahdi <93738112+BKR779@users.noreply.github.com>
2023-04-28 04:09:03 +0300

    refactor(pages): remove the word page from the default pages

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -72,7 +72,7 @@ export default function Footer() {
                 </Link>
               </li>
               <li className='mb-4'>
-                <Link href='/listing-page' className='hover:underline'>
+                <Link href='/listing' className='hover:underline'>
                   Start Selling
                 </Link>
               </li>
