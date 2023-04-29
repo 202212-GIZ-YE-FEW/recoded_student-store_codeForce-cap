003500119c77acf1fe4705d36dd2d1b8ce74ada1
Abobakr Mahdi <93738112+BKR779@users.noreply.github.com>
2023-04-29 17:57:46 +0300

    refactor(listingitems) update the listingitems valedation

--- src/utils/schemaValidations/listingItems.jsx
+++ src/utils/schemaValidations/listingItems.jsx
@@ -1,29 +1,15 @@
 import * as Yup from "yup"
 
 export const listingsValidation = Yup.object().shape({
-  // primaryImage: Yup.object().shape({
-  //   file: Yup.mixed().optional(),
-  //   url: Yup.string().url().optional(),
-  // }),
-  // secondaryImage: Yup.object().shape({
-  //   file: Yup.mixed().optional(),
-  //   url: Yup.string().url().optional(),
-  // }),
-  // tertiaryImage: Yup.object().shape({
-  //   file: Yup.mixed().optional(),
-  //   url: Yup.string().url().optional(),
-  // }),
-  type: Yup.string().required("Type is required"),
-  category: Yup.string().required("Category is required"),
-  productName: Yup.string()
-    .matches(
-      /^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$/,
-      "Please enter a valid School name"
-    )
-    .required("Product name is required"),
-  description: Yup.string().required("Description is required"),
+  type: Yup.string().required(
+    "Choose a Type the students need to know what you list for them !"
+  ),
+  category: Yup.string().required("What is the category of your list ?"),
+  productName: Yup.string().required("What is the product name ?"),
+  description: Yup.string().required("Please describe your listing"),
   location: Yup.string().required("Location is required"),
   price: Yup.number()
-    .required("Price is required")
-    .min(0, "Price must be greater than or equal to 0"),
+    .required("How much is it coast ?")
+    .min(0, "Price must be greater than or equal to 0")
+    .max(1000, "Price must be less than or equal to 1000 $"),
 })
