2b0a3fc6fc7b067a56de3e5d310b86729cd4cdd8
Abobakr Mahdi <93738112+BKR779@users.noreply.github.com>
2023-04-29 04:26:22 +0300

    docs(utils): add the listingItems product name validation
    in this pull request we added the all necessary validation to the sign in and the listing items and
    the rest of the needed component to be validate
    
    BREAKING CHANGE: new library with new utils with extractings and addings
    
    #22

--- src/utils/schemaValidations/listingItems.jsx
+++ src/utils/schemaValidations/listingItems.jsx
@@ -15,7 +15,12 @@ export const listingsValidation = Yup.object().shape({
   // }),
   type: Yup.string().required("Type is required"),
   category: Yup.string().required("Category is required"),
-  productName: Yup.string().required("Product name is required"),
+  productName: Yup.string()
+    .matches(
+      /^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$/,
+      "Please enter a valid School name"
+    )
+    .required("Product name is required"),
   description: Yup.string().required("Description is required"),
   location: Yup.string().required("Location is required"),
   price: Yup.number()
