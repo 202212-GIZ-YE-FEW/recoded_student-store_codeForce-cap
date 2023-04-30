580308ffc6386a1c7f0c21c7f95f4ce6bbbc7a69
Abobakr Mahdi <93738112+BKR779@users.noreply.github.com>
2023-04-30 21:56:55 +0300

    refactor(store): add useAuth hook to handle the users signin or signup and never redirect them if they are gsigned in the website

--- src/utils/store.jsx
+++ src/utils/store.jsx
@@ -1,4 +1,4 @@
-import { createContext, useEffect, useState } from "react"
+import { createContext, useContext, useEffect, useState } from "react"
 
 import { auth } from "./firebase/config"
 
@@ -7,11 +7,13 @@ export const StoreContext = createContext()
 export const StoreProvider = ({ children }) => {
   const [user, setUser] = useState(null)
   const [loading, setLoading] = useState(true)
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
 
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged((user) => {
       setUser(user)
       setLoading(false)
+      setIsLoggedIn(user !== null)
     })
 
     return () => unsubscribe()
@@ -23,4 +25,16 @@ export const StoreProvider = ({ children }) => {
   }
 
   return <StoreContext.Provider value={value}>{children}</StoreContext.Provider>
+}
+
+export const useAuth = () => {
+  const { user, loading } = useContext(StoreContext)
+
+  const isLoggedIn = user !== null
+
+  return {
+    user,
+    loading,
+    isLoggedIn,
+  }
 }
