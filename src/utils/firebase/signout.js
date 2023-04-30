04ba22da4daf35590f6001f10da45ec7791adb38
Abobakr Mahdi <93738112+BKR779@users.noreply.github.com>
2023-05-01 02:20:59 +0300

    refactor(signout): update the signout so that it contain the signout functionalety with the button so that we can control the notefiers

--- src/utils/firebase/signout.js
+++ src/utils/firebase/signout.js
@@ -1,12 +1,35 @@
 import { getAuth, signOut } from "firebase/auth"
+import { useRouter } from "next/router"
+import { TbLogout } from "react-icons/tb"
 import { toast } from "react-toastify"
 
-export default async function SignOut() {
+function SignOut() {
+  const router = useRouter()
   const auth = getAuth()
-  try {
-    await signOut(auth)
-    toast.success("User signed out successfully")
-  } catch (e) {
-    toast.error("Failed to sign out user")
+
+  const handleSignOut = async () => {
+    try {
+      toast.warn("Please wait")
+      await signOut(auth)
+        .then(() => {
+          router.push("/")
+        })
+        .then(() => {
+          toast.success("User signed out successfully")
+        })
+    } catch (e) {
+      toast.error("Failed to sign out user")
+    }
   }
+
+  return (
+    <span className='block px-4 py-2 hover:bg-gray-100 w-full text-left cursor-pointer'>
+      <span className='flex items-center gap-2'>
+        <button onClick={handleSignOut}>Logout</button>
+        <TbLogout />
+      </span>
+    </span>
+  )
 }
+
+export default SignOut
