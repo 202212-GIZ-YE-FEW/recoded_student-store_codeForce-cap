f1006acf080911326e7c2199c4269e5ce95b8c01
MarwanBz <69892218+MarwanBz@users.noreply.github.com>
2023-05-01 09:09:08 +0300

    feat: integrate fav component into the web app && link favorites page to navbar && update tests

---
+++ src/pages/favorites.jsx
@@ -0,0 +1,10 @@
+import FavProducts from "@/components/FavProducts"
+import RootLayout from "@/layout/root/RootLayout"
+
+export default function favorites() {
+  return (
+    <RootLayout>
+      <FavProducts />
+    </RootLayout>
+  )
+}
