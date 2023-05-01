50bffa8994965d4c9967bfcca005b51a796e9fd0
Abobakr Mahdi <93738112+BKR779@users.noreply.github.com>
2023-05-01 05:59:57 +0300

    refactor(listingitems): protuct listingitems page from accessing with out signup or signin

--- src/pages/listing.jsx
+++ src/pages/listing.jsx
@@ -1,10 +1,23 @@
 import { serverSideTranslations } from "next-i18next/serverSideTranslations"
+import { useRouter } from "next/router"
+import { useEffect } from "react"
+import { toast } from "react-toastify"
 
 import ListingItems from "@/components/ListingItems"
 
 import RootLayout from "@/layout/root/RootLayout"
+import { useAuth } from "@/utils/store"
 
 function ListingPage() {
+  const router = useRouter()
+  const { isLoggedIn } = useAuth()
+  useEffect(() => {
+    if (!isLoggedIn) {
+      router.replace("/").then(() => {
+        toast.info("Hey you i think you have to sign up or sign in first !")
+      })
+    }
+  }, [isLoggedIn, router])
   return (
     <RootLayout>
       <ListingItems />
