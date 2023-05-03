e18715fd097fdff5d085e1b4dddeb4d0d6ff424d
Abobakr Mahdi <93738112+BKR779@users.noreply.github.com>
2023-05-03 08:20:50 +0300

    refactor(listingitems): impelement the listing items to be using the default image that came from the firebase

--- src/components/ListingItems/ListingItems.jsx
+++ src/components/ListingItems/ListingItems.jsx
@@ -3,13 +3,14 @@ import { addDoc, collection } from "firebase/firestore"
 import { getDownloadURL, ref, uploadBytes } from "firebase/storage"
 import { useTranslation, withTranslation } from "next-i18next"
 import Image from "next/image"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { ToastContainer, toast } from "react-toastify"
 
 import "react-toastify/dist/ReactToastify.css"
 
 import { auth, db, storage, timestamp } from "@/utils/firebase/config"
 import { listingsValidation } from "@/utils/schemaValidations/listingItems"
+import { useDefaultImage } from "@/utils/store"
 
 import Button from "../button"
 import Highlighter from "../highlighter"
@@ -22,10 +23,10 @@ function ListingItems() {
   const [errors, setErrors] = useState({})
   // Form info
   const [formData, setFormData] = useState({
-    primaryImage: { file: null, url: "/images/emptyImage.png" },
-    secondaryImage: { file: null, url: "/images/emptyImage.png" },
-    tertiaryImage: { file: null, url: "/images/emptyImage.png" },
-    quaternaryImage: { file: null, url: "/images/emptyImage.png" },
+    primaryImage: { file: null, url: "" },
+    secondaryImage: { file: null, url: "" },
+    tertiaryImage: { file: null, url: "" },
+    quaternaryImage: { file: null, url: "" },
     type: "",
     category: "",
     productName: "",
@@ -34,6 +35,20 @@ function ListingItems() {
     price: "",
     createdAt: timestamp,
   })
+
+  const defaultImageURL = useDefaultImage()
+
+  useEffect(() => {
+    if (defaultImageURL) {
+      setFormData((prevFormData) => ({
+        ...prevFormData,
+        primaryImage: { file: null, url: defaultImageURL },
+        secondaryImage: { file: null, url: defaultImageURL },
+        tertiaryImage: { file: null, url: defaultImageURL },
+        quaternaryImage: { file: null, url: defaultImageURL },
+      }))
+    }
+  }, [defaultImageURL])
 
   // Inputs verifier and handler
   const inputsHandler = (event) => {
