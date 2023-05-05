a53639cb9bf3a59bad067bdc8e7e5b82831d07d9
Abobakr Mahdi <93738112+BKR779@users.noreply.github.com>
2023-05-05 21:36:50 +0300

    refactor(signin): protect the pge from access it if the user signed in

--- src/pages/signin.jsx
+++ src/pages/signin.jsx
@@ -1,10 +1,28 @@
+import { useTranslation } from "next-i18next"
 import { serverSideTranslations } from "next-i18next/serverSideTranslations"
+import { useRouter } from "next/router"
+import { useEffect } from "react"
+import { toast } from "react-toastify"
 
 import Signin from "@/components/Signin"
 
 import RootLayout from "@/layout/root/RootLayout"
+import { useAuth, useProfileData } from "@/utils/store"
 
 export default function SignInPage() {
+  const { t } = useTranslation("signup")
+  const router = useRouter()
+  const { isLoggedIn } = useAuth()
+  const userName = useProfileData()
+  useEffect(() => {
+    if (isLoggedIn) {
+      router.replace("/").then(() => {
+        toast.info(
+          t("hey") + ` ${userName?.firstName || t("you")} ` + t("signed")
+        )
+      })
+    }
+  }, [isLoggedIn, router, userName, t])
   return (
     <RootLayout>
       <Signin />
@@ -15,7 +33,7 @@ export default function SignInPage() {
 export async function getStaticProps({ locale }) {
   return {
     props: {
-      ...(await serverSideTranslations(locale, ["signin", "common"])),
+      ...(await serverSideTranslations(locale, ["signin", "common", "signup"])),
       // Will be passed to the page component as props
     },
   }
