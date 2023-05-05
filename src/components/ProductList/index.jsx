1b82c47c6459e4ad9bd8126f4f4a95302d084291
messam10 <memaq1998.10.7@gmail.com>
2023-05-05 18:39:21 +0300

    feat(product list): add link for each product

--- src/components/ProductList/index.jsx
+++ src/components/ProductList/index.jsx
@@ -3,6 +3,7 @@ import { withTranslation } from "next-i18next"
 import { AiOutlineHeart } from "react-icons/ai"
 
 import Products from "./products"
+import Link from "next/link"
 
 function ProductList({ selectedFilter, t }) {
   // Remove duplicates from the products array
@@ -24,39 +25,40 @@ function ProductList({ selectedFilter, t }) {
         dir={t("language") === "ar" ? "rtl" : "ltr"}
       >
         {categoryFilter.map((product) => (
-          <div
-            key={product.id}
-            className='mx-3 mb-10 border rounded-lg cart-animation'
-          >
-            <div className='relative overflow-hidden'>
-              <Image
-                src={product.images}
-                alt={product.name}
-                width={258}
-                height={211.41}
-                className='rounded-t-lg shadow-lg w-full '
-              />
-              <div className='absolute bottom-2 right-2 z-10'>
-                <button className='flex items-center justify-center w-8 h-8 bg-white text-red-500 rounded-full shadow-md hover:text-red-500 transition-colors duration-300 ease-in-out'>
-                  <AiOutlineHeart />
-                </button>
-              </div>
-            </div>
-            <div className='mx-3 text-center'>
-              <div className='info flex justify-between my-4 mx-3'>
-                <div className='text-left'>
-                  <h2 className='font-semibold'>{product.name}</h2>
-                  <p className='font-extralight text-xs rtl:text-right'>
-                    {product.category}
-                  </p>
-                </div>
-                <div>
-                  <h2 className='font-extrabold text-xl'>${product.price}</h2>
-                  <p className='font-extralight text-xs'>{product.made_city}</p>
+          <Link href={`/products/${product.id}`} key={product.id}>
+            <div className='mx-3 mb-10 border rounded-lg cart-animation'>
+              <div className='relative overflow-hidden'>
+                <Image
+                  src={product.images}
+                  alt={product.name}
+                  width={258}
+                  height={211.41}
+                  className='rounded-t-lg shadow-lg w-full '
+                />
+                <div className='absolute bottom-2 right-2 z-10'>
+                  <button className='flex items-center justify-center w-8 h-8 bg-white text-red-500 rounded-full shadow-md hover:text-red-500 transition-colors duration-300 ease-in-out'>
+                    <AiOutlineHeart />
+                  </button>
                 </div>
               </div>
+              <div className='mx-3 text-center'>
+                <div className='info flex justify-between my-4 mx-3'>
+                  <div className='text-left'>
+                    <h2 className='font-semibold'>{product.name}</h2>
+                    <p className='font-extralight text-xs rtl:text-right'>
+                      {product.category}
+                    </p>
+                  </div>
+                  <div>
+                    <h2 className='font-extrabold text-xl'>${product.price}</h2>
+                    <p className='font-extralight text-xs'>
+                      {product.made_city}
+                    </p>
+                  </div>
+                </div>
+              </div>
             </div>
-          </div>
+          </Link>
         ))}
       </div>
     </div>
