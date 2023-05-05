5b186dacc9e42d43b2d73649894cf271c80ad54f
Abobakr Mahdi <93738112+BKR779@users.noreply.github.com>
2023-05-06 02:19:24 +0300

    reafactor(userListings): impelement the user listings in the page so that it will fetch the user listings in his profile

--- src/components/UserListings/UserListings.jsx
+++ src/components/UserListings/UserListings.jsx
@@ -1,9 +1,23 @@
 import { withTranslation } from "next-i18next"
 import Image from "next/image"
+import { Link } from "react-router-dom"
 
-import products from "../ProductList/products"
+import { useUserListings } from "@/utils/store"
 
 function UserListings({ t }) {
+  const { data, loading } = useUserListings()
+  if (loading) {
+    return <h2>Loading...</h2>
+  }
+  if (data.length === 0) {
+    return (
+      <h1>
+        Ohh u did not list anything till now why do not u go to{" "}
+        <Link href='/listing'>List a new item</Link>{" "}
+      </h1>
+    )
+  }
+  console.log(data)
   return (
     <section
       className='flex flex-col lg:flex-row h-[617px] md:h-[784px] lg:h-[100%] overflow-y-auto lg:bg-gradient-to-l from-zinc-800 to-slate-300'
@@ -11,22 +25,22 @@ function UserListings({ t }) {
     >
       <div className=' w-full mx-10'>
         <div className='grid lg:grid-cols-3 md:grid-cols-2 gap-5 px-8 py-16'>
-          {products({ t }).map((product) => (
+          {data.map((product) => (
             <div
               key={product.id}
               className='lg:bg-white rounded-lg pb-7 cart-animation'
             >
               <Image
                 className='rounded-t-lg shadow-lg w-full'
-                src='/images/productImg.png'
-                alt='...'
+                src={product.primaryImage.url}
+                alt={product.productName}
                 width={1920}
                 height={1080}
               />
               <div className='mx-2'>
                 <div className='flex justify-between my-4'>
                   <div className='text-left'>
-                    <h2 className='font-semibold'>{product.name}</h2>
+                    <h2 className='font-semibold'>{product.productName}</h2>
                     <p className='font-extralight text-xs'>
                       {product.category}
                     </p>
