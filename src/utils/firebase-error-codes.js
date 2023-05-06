831b3c7da1418668eb353b430e213fa075e7be6a
Abobakr Mahdi <93738112+BKR779@users.noreply.github.com>
2023-05-06 23:37:35 +0300

    refactor(firebase errors):  add the requires recent login in the firebase error codes

--- src/utils/firebase-error-codes.js
+++ src/utils/firebase-error-codes.js
@@ -1,7 +1,8 @@
 // Error codes for Firebase Auth
-const ERROR_CODES = {
+export const ERROR_CODES = {
   "auth/email-already-in-use": "This email is already in use",
   "auth/invalid-email": "Invalid email",
   "auth/weak-password": "Password is too weak",
   "auth/wrong-password": "Incorrect email or password",
+  "auth/requires-recent-login": "You have to re enter your code",
 }
