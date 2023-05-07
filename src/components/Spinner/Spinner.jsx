b621e3c5e4a3aebd00c82dbf1dfd2805694093f6
Abobakr Mahdi <93738112+BKR779@users.noreply.github.com>
2023-05-07 08:12:51 +0300

    style(spinner): set a new text that will be displayed next to the spinner

--- src/components/Spinner/Spinner.jsx
+++ src/components/Spinner/Spinner.jsx
@@ -2,9 +2,10 @@ import { HashLoader } from "react-spinners"
 
 import styles from "./spinner.module.css"
 
-const Spinner = () => (
+const Spinner = ({ text }) => (
   <div className={styles.wrapper}>
     <HashLoader color='#eeeeee' size={80} />
+    {text && <p className={styles.loadingText}>{text}</p>}
   </div>
 )
 
