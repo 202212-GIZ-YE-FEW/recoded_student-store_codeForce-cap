92edd09a98123d0291bd7f8445f29bdaee7dbe58
Abobakr Mahdi <93738112+BKR779@users.noreply.github.com>
2023-05-09 06:47:20 +0300

    docs(project): run necessary commands to sort the codes

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,7 +1,7 @@
-import { useTranslation } from "next-i18next"
-import { serverSideTranslations } from "next-i18next/serverSideTranslations"
 import Image from "next/image"
 import Link from "next/link"
+import { useTranslation } from "next-i18next"
+import { serverSideTranslations } from "next-i18next/serverSideTranslations"
 
 import RootLayout from "@/layout/root/RootLayout"
 
