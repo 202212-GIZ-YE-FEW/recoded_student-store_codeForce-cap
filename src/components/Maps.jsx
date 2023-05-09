63a74d397ddfed79acde880aec0ce2a5ff09a302
Abobakr Mahdi <93738112+BKR779@users.noreply.github.com>
2023-05-09 22:23:07 +0300

    refactor(maps): add the location marker to the map

--- src/components/Maps.jsx
+++ src/components/Maps.jsx
@@ -1,6 +1,15 @@
+import L from "leaflet"
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet"
 
 import "leaflet/dist/leaflet.css"
+
+const customIcon = new L.Icon({
+  iconUrl: "/images/location.svg",
+  iconRetinaUrl: "/images/location.svg",
+  iconAnchor: [15, 50],
+  popupAnchor: [0, -50],
+  iconSize: [30, 50],
+})
 
 export default function Maps() {
   const sanaaCoordinates = [15.3694, 44.191]
@@ -25,7 +34,7 @@ export default function Maps() {
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
       />
-      <Marker position={sanaaCoordinates}>
+      <Marker position={sanaaCoordinates} icon={customIcon}>
         <Popup>Sana&apos;a, Yemen</Popup>
       </Marker>
     </MapContainer>
