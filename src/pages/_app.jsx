4d639d0715150f7aa903e9f93a63029749365b7e
Abobakr Mahdi <93738112+BKR779@users.noreply.github.com>
2023-05-11 14:11:26 +0300

    feat(errorbundary):add the error boundary to save the web form stoping

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -3,14 +3,17 @@ import { appWithTranslation } from "next-i18next"
 import "../styles/globals.css"
 
 import { AuthChecker } from "@/utils/AuthChecker"
+import ErrorBoundary from "@/utils/ErrorBundary"
 import { StoreProvider } from "@/utils/store"
 
 function MyApp({ Component, pageProps }) {
   return (
     <div className='bg-iceblue'>
-      <StoreProvider>
-        <AuthChecker Component={Component} pageProps={pageProps} />
-      </StoreProvider>
+      <ErrorBoundary>
+        <StoreProvider>
+          <AuthChecker Component={Component} pageProps={pageProps} />
+        </StoreProvider>
+      </ErrorBoundary>
     </div>
   )
 }
---
+++ src/utils/ErrorBundary.jsx
@@ -0,0 +1,31 @@
+import React from "react"
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { hasError: false }
+  }
+  static getDerivedStateFromError(error) {
+    return { hasError: true }
+  }
+  componentDidCatch(error, errorInfo) {
+    console.log({ error, errorInfo })
+  }
+  render() {
+    if (this.state.hasError) {
+      return (
+        <div>
+          <h2>Oops, there is an error!</h2>
+          <button
+            type='button'
+            onClick={() => this.setState({ hasError: false })}
+          >
+            Try again?
+          </button>
+        </div>
+      )
+    }
+    return this.props.children
+  }
+}
+
+export default ErrorBoundary
